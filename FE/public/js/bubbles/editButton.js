c26513c6bfc4d30ca2af3a339d7cf5f3edd23c0a
CatScan <mariroth60@gmail.com>
2023-03-21 16:47:31 +0200

    Cleaning editButtons.js

--- FE/public/js/bubbles/editButton.js
+++ FE/public/js/bubbles/editButton.js
@@ -10,24 +10,20 @@ $(document).on('click', '#user-message-edit, #assistant-message-edit', function(
     // sendMessageIndex(numberTosend,newText)
     }
 
-
-function enableEditMode(divToEdit,numberTosend,) {
   // Replace the div content with an input element that contains the current text
+function enableEditMode(divToEdit,numberTosend,) {
   const currentText = divToEdit.text();
   const inputElement = $('<input>').val(currentText);
   divToEdit.empty().append(inputElement);
-  let y = divToEdit.attr('data-messages') 
-  x = `currentMessageSetIndex${y}`
 
   // Create cancel and approve buttons
   const cancelButton = $('<button>').text('Cancel');
   const approveButton = $('<button>').text('Approve');
   const backwards = $('<button>').text('->').attr('id', 'backwards'); 
   backwards.attr('data-messages', divToEdit.attr('data-messages'));
-  backwards.attr(x,0);
   const forwards = $('<button>').text('<-').attr('id', 'forwards');
   forwards.attr('data-messages', divToEdit.attr('data-messages'));
-  forwards.attr(x,0);
+
   // When the user clicks the "Cancel" button, disable the edit mode
   cancelButton.on('click', () => {
     saveChanges(divToEdit, currentText);
@@ -35,11 +31,11 @@ function enableEditMode(divToEdit,numberTosend,) {
 
   // When the user clicks the "Approve" button, update the div content with the new text and disable the edit mode
   approveButton.on('click', () => {
-    let y = divToEdit.attr('data-messages') 
-        x = `currentMessageSetIndex[${y}]`
+    let currentDiv = divToEdit.attr('data-messages') 
+    currentMessageArrayIndex = `currentMessageSetIndex[${currentDiv}]`
     
-    if(!sessionStorage.getItem(x)){
-      sessionStorage.setItem(x, 0);
+    if(!sessionStorage.getItem(currentMessageArrayIndex)){
+      sessionStorage.setItem(currentMessageArrayIndex, 0);
     }
     const newText = inputElement.val();
     divToEdit.text(newText);
@@ -54,40 +50,33 @@ function enableEditMode(divToEdit,numberTosend,) {
     divToEdit.parent().find('#forwards').prop('disabled', true);
     divToEdit.parent().find('#backwards').prop('disabled', false);
     divToEdit.parent().nextAll().remove();
-    sessionStorage.setItem(x, +sessionStorage.getItem(x) + 1);
+    sessionStorage.setItem(currentMessageArrayIndex, +sessionStorage.getItem(currentMessageArrayIndex) + 1);
   });
   
   // Append the buttons to the div
   divToEdit.append(cancelButton);
   divToEdit.append(approveButton);
-  // Focus on the input element and select its contents
   inputElement.focus().select();
 }
-function saveChanges(divToEdit, newText) {
   // Replace the input element and buttons with a regular div that contains the new text
+function saveChanges(divToEdit, newText) {
   const newDivElement = $('<div>').text(newText);
   divToEdit.empty().append(newDivElement);
 }
-
-function saveMessages(divToEdit) {
   // Get the container ID and role from the container element
-  const containerId = divToEdit.parent().attr('id');
-  const role = containerId.split('-')[0];
-  
+function saveMessages(divToEdit) {
   // Get the messages from all subsequent containers with the same role
   const messages = [];
   divToEdit.parent().nextAll().each(function() {
     const id = $(this).attr('id');
-    if (id.startsWith(role)) {
+    const role = id.split('-')[0];
+    console.log(role);
       const messageText = this.innerText.trim();
       messages.push({ role: role, content: messageText });
-    }
   });
-  
-  let x = divToEdit.attr('data-messages')
-  const messageSetsKey = `messageSets${x}`;
+  let currentMessageArrayIndex = divToEdit.attr('data-messages')
+  const messageSetsKey = `messageSets${currentMessageArrayIndex}`;
   let messageSets = JSON.parse(sessionStorage.getItem(messageSetsKey)) || [];
-
   // Get the index for the new message set
   messageSets.push({ messages: messages });
   sessionStorage.setItem(messageSetsKey, JSON.stringify(messageSets));
@@ -96,21 +85,21 @@ function saveMessages(divToEdit) {
 }
 
 
+
 $(document).on('click', '#backwards , #forwards', function() {
-  currentButton = $(this).attr('id')
-  currentContainer = $(this).closest('#user-bubble-container, #assistant-bubble-container')
-  oppositeButton = currentContainer.find('#backwards , #forwards')
+  const currentButton = $(this).attr('id')
+  const currentContainer = $(this).closest('#user-bubble-container, #assistant-bubble-container')
+  const oppositeButton = currentContainer.find('#backwards , #forwards')
 
-  messagesSetlocation = currentContainer.find('#user-bubble, #assistant-bubble');
-  messageSetIndex = messagesSetlocation.attr('data-messages');
-  sessionMessagesArray = `messageSets${messageSetIndex}`
-  currentMessageSetIndex = `currentMessageSetIndex[${messageSetIndex}]`
-  messageSets = JSON.parse(sessionStorage.getItem(sessionMessagesArray));
-  indexLength = messageSets.length 
-
+  const messagesSetlocation = currentContainer.find('#user-bubble, #assistant-bubble');
+  const messageSetIndex = messagesSetlocation.attr('data-messages');
+  const sessionMessagesArray = `messageSets${messageSetIndex}`
+  const currentMessageSetIndex = `currentMessageSetIndex[${messageSetIndex}]`
+  const messageSets = JSON.parse(sessionStorage.getItem(sessionMessagesArray));
+  const indexLength = messageSets.length 
   function goDirection(direction) {
-    const current = +sessionStorage.getItem(currentMessageSetIndex);
-    const newCurrent = direction === 'backwards' ? current - 1 : current + 1;
+    current = +sessionStorage.getItem(currentMessageSetIndex);
+    newCurrent = direction === 'backwards' ? current - 1 : current + 1;
     sessionStorage.setItem(currentMessageSetIndex, newCurrent);
   }
 
@@ -119,11 +108,11 @@ $(document).on('click', '#backwards , #forwards', function() {
 
   switch(currentButton) {
     case 'backwards':
-      (checkIf(currentMessageSetIndex) > 0 ) ? (goDirection('backwards'), populateConsole(), $(oppositeButton).prop('disabled', false) ) : null;
+      (checkIf(currentMessageSetIndex)  > 0 ) ? (goDirection('backwards'), populateConsole(), $(oppositeButton).prop('disabled', false) ) : null;
       (checkIf(currentMessageSetIndex) <= 0 ) ? $(this).prop('disabled', true) : null;
       break;
     case 'forwards':
-    (checkIf(currentMessageSetIndex)  <  indexLength) ? (goDirection('forwards'), populateConsole(), $(oppositeButton).prop('disabled', false)) : null;
+    (checkIf(currentMessageSetIndex)  < indexLength) ? (goDirection('forwards'), populateConsole(), $(oppositeButton).prop('disabled', false)) : null;
     (checkIf(currentMessageSetIndex) >= indexLength) ? $(this).prop('disabled', true) : null;
       break;
 }
