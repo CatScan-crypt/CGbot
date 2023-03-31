4f939fe5b61e4ce5720531f26404f6cf878f0f54
CatScan <mariroth60@gmail.com>
2023-03-31 13:06:39 +0300

    Update-0.131411:

--- FE/public/js/bubbles/historyButton.js
+++ FE/public/js/bubbles/historyButton.js
@@ -1,7 +1,5 @@
-// Set initial value for currentMessageSetIndex
 sessionStorage.setItem("currentMessageSetIndex", 0);
 
-// Event listener for the click event on navigation buttons
 document.addEventListener("click", (event) => {
   if (event.target.id === "backwards" || event.target.id === "forwards") {
     navigateMessages(event.target.id);
@@ -9,27 +7,15 @@ document.addEventListener("click", (event) => {
 });
 
 function navigateMessages(clickedButtonId) {
-  // Determine the direction for navigation based on the clicked button's ID
   const direction = clickedButtonId === "backwards" ? -1 : 1;
-
-  // Retrieve the current message set index from session storage and parse it as an integer
   const currentMessageSetIndex = parseInt(sessionStorage.getItem("currentMessageSetIndex"));
-
-  // Retrieve the messages array from session storage and parse it as a JSON object
   const messagesArray = JSON.parse(sessionStorage.getItem("MessagesArrayCounter0"));
-
-  // Calculate the new index by adding the direction value to the current message set index
   const newIndex = currentMessageSetIndex + direction;
 
-  // Check if the new index is within the bounds of the messages array
+
   if (newIndex >= 0 && newIndex <= messagesArray.length - 1) {
-    // Update the current message set index in session storage with the new index value
     sessionStorage.setItem("currentMessageSetIndex", newIndex);
-
-    // Populate the output area with messages from the message set at the new index
     populateOutput(messagesArray[newIndex].messages);
-
-    // Update the disabled state of the navigation buttons based on the new index
     updateButtonDisabledState("backwards", newIndex <= 0);
     updateButtonDisabledState("forwards", newIndex >= messagesArray.length - 1);
   }
